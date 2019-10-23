175a34c894732b90d6200f52d1eb7a2649b9e5f5
Andrey Leontev <leontev.andrey.s@gmail.com>
2019-10-23 13:59:35 +1000

    add main funcitons to routes

--- route.rb
+++ route.rb
@@ -1,3 +1,49 @@
 class Route
-  #TODO add attrs, relations, methods
+  attr_accessor :route
+
+	def initialize(stations = [])
+		if stations.size >= 2
+      @stations = stations
+		# @route = []
+			puts "Route #{@stations.first} - #{@stations.last} created"
+    else
+      puts "There are few stations in the route"
+    end
+	end
+
+	def start_route
+		puts "The first station on this route #{@stations[0]}"
+	end
+
+	def end_route
+		puts "The last station on this route  #{@stations[-1]}"
+	end
+
+	def middle_route
+		puts "Intermediate stations on this route #{@stations[1..-2]}"
+	end
+
+	def add(station)
+		@stations.insert(-2, station)
+		puts "New station (#{station}) added to route"
+	end
+
+	def delete(station)
+		if @stations.include?(station)
+			@stations.delete(station)
+			puts "Station #{station} removed from route"
+		else
+			puts "Station #{station} is not on the route"
+		end
+	end
+
+	def list
+	  puts "Direction from #{@stations.first} to #{@stations.last}"
+    @stations.each_with_index{ |station, index| puts "#{index + 1}. #{station}"}
+	end
+
+	def stations
+    @stations
+  end
+
 end
